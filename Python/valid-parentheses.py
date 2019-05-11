466373f8387c6a751639cc65526cf69a0f27db31
Chris Chen <utcsox@gmail.com>
2019-05-11 09:24:23 -0700

    Create valid-parentheses.py
    solution valid paran

---
+++ Python/valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lookup = {'{': '}', '(': ')', '[': ']'}
+        L = []
+    
+        for char in s:
+            print(char, len(L))
+            if char in lookup:
+                L.append(char)
+            elif len(L) == 0 or lookup[L.pop()] != char:
+                return False
+        return len(L) == 0

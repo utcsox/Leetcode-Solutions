09134c52b22da36610d61a83ec368ac7ce4df42f
Chris Chen <utcsox@gmail.com>
2019-07-29 16:49:38 -0700

    Update letter-combinations-of-a-phone-number.py
    adding #17 DFS solution

--- Python/letter-combinations-of-a-phone-number.py
+++ Python/letter-combinations-of-a-phone-number.py
@@ -20,3 +20,26 @@ class Solution1:
             ans = tmp
       
         return ans
+# DFS
+class Solution2: 
+    def letterCombinations(self, digits: str)-> List[str]:
+        dictionary = {'2': 'abc',
+              '3': 'def',
+              '4': 'ghi',
+              '5': 'jkl',
+              '6': 'mno',
+              '7': 'pqrs',
+              '8': 'tuv',
+              '9': 'wxyz'}
+        
+        def bfs(tmp, next_digits):
+            if len(next_digits) == 0:
+                output.append(tmp)
+            else:
+                for d in dictionary[next_digits[0]]:
+                    bfs(tmp + d, next_digits[1:])
+        output = []        
+        if digits:
+            bfs("", digits)
+        return output
+

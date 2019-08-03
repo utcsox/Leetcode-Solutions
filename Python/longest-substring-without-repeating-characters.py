6817865599b1eb0360c3c85194ac692820e53bc8
Chris Chen <utcsox@gmail.com>
2019-08-02 23:18:35 -0700

    Create longest-substring-without-repeating-characters.py
    add solution #3

---
+++ Python/longest-substring-without-repeating-characters.py
@@ -0,0 +1,53 @@
+class Solution1:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        if len(s) == 0:
+            return 0
+        
+        
+        max_length = 0
+        
+        for i in range(len(s)):
+            tempstr = '' + s[i]
+            length = 1
+            
+            for j in range(len(s))[i+1:]:
+                
+                if s[j] not in tempstr:
+                    tempstr += s[j]
+                    length += 1
+                else:
+                    if length > max_length:
+                        max_length = length
+
+                    break
+            if length >= max_length:
+                max_length = length
+                    
+        return max_length
+        
+        
+class Solution2:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        if len(s) == 0:
+            return 0
+        tempstr = ''
+        maxLen = 0
+        
+        for char in s:
+            if char not in tempstr:
+                tempstr +=  char
+                if len(tempstr) > maxLen:
+                    maxLen = len(tempstr)
+            else: 
+                tempstr = tempstr[tempstr.index(char) + 1 : ] + char
+            
+        return maxLen
+                
+                
+        

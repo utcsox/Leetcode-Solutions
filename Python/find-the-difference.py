74acc63e14508e046cde94e71432ade7f7b2771f
Chris Chen <utcsox@gmail.com>
2019-08-17 14:51:03 -0700

    Update find-the-difference.py
    Add a list based solution

--- Python/find-the-difference.py
+++ Python/find-the-difference.py
@@ -15,3 +15,11 @@ class Solution1:
                 return char
             else:
                 dict_item[char] -= 1
+class Solution2:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t = list(t)
+        s = list(s)
+        for i in s:
+            t.remove(i)
+        return t[0]
+                    

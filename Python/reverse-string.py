d4d49656543e7756ab3af54c20932245570e7ffd
Chris Chen <utcsox@gmail.com>
2020-05-11 19:29:08 -0700

    Update reverse-string.py
    add recursive solution

--- Python/reverse-string.py
+++ Python/reverse-string.py
@@ -16,4 +16,18 @@ class Solution2(object):
         
         for i in range(len(s)//2):
             s[i], s[-i-1] = s[-i-1], s[i]
-        
+            
+class Solution3:
+def reverseString(self, s: List[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    def helper(start, end, ls):
+        if start >= end:
+            return
+        else:
+            ls[start], ls[end] = ls[end], ls[start]
+
+        return helper(start+1, end-1, ls)
+
+    helper(0, len(s)-1, s)     

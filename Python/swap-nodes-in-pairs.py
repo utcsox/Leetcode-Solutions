f23321c2623fadc8ae179f2f3e5f44435fb174fa
Chris Chen <utcsox@gmail.com>
2020-05-13 22:26:19 -0700

    Update swap-nodes-in-pairs.py
    Add recursive solution

--- Python/swap-nodes-in-pairs.py
+++ Python/swap-nodes-in-pairs.py
@@ -19,4 +19,19 @@ class Solution1:
             head = first_node.next
             
         return dummy.next
+    
+class Solution2:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        
+        if not head or not head.next:
+            return head
+        
+        else:
+            first_node = head.
+            second_node = head.next
+        
+        first_node.next = self.swapPairs(second_node.next)
+        second_node.next = first_node
+        
+        return second_node
         

1b1c9e6d85e26cac21522341479c24132b32d933
Chris Chen <utcsox@gmail.com>
2020-05-19 20:41:42 -0700

    Update add-binary.py
    Add second solution for #67 add Two binary

--- Python/add-binary.py
+++ Python/add-binary.py
@@ -1,3 +1,23 @@
 class Solution1:
     def addBinary(self, a: str, b: str) -> str:
         return bin(int(a,2) + int(b,2))[2:]
+
+class Solution2:
+    def addBinary(self, a: str, b: str) -> str:
+        first = len(a)-1
+        second = len(b)-1
+
+        carry, total, result = 0, 0, ''
+        while (first >= 0 or second >=0):
+            total = carry
+            if first >= 0:
+                total = total + int(a[first])
+            if second >= 0:
+                total = total + int(b[second])
+            result = result + str(total%2)
+            carry = int(total/2)
+            first = first-1
+            second = second -1
+        if carry != 0: 
+            result = result + str(carry)
+        return result[::-1]

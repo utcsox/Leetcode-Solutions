a688d2d602211e840e9fac666d43afb99a443b83
Chris Chen <utcsox@gmail.com>
2020-05-23 12:26:51 -0700

    Update move-zeroes.py
    Add two more approaches to this problems

--- Python/move-zeroes.py
+++ Python/move-zeroes.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution2:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -7,3 +7,28 @@ class Solution:
             if nums[i] == 0:
                 nums.extend([nums[i]])
                 del nums[i]
+        
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pointer = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[pointer] =  nums[i]
+                pointer += 1
+        for i in range(pointer, len(nums)):
+            nums[i] = 0
+            
+class Solution3:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in range(len(nums))[::-1]:
+            if nums[i] == 0:
+                del nums[i]
+                nums.append(0)
+        return nums

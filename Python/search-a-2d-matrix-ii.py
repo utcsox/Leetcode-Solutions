12f163f46603b6181efcbdf8ca519050a7c9b5dc
Chris Chen <utcsox@gmail.com>
2020-06-07 18:37:26 -0700

    Update search-a-2d-matrix-ii.py
    Add Search Space Reduction solution into 240.  Search a 2D Matrix II

--- Python/search-a-2d-matrix-ii.py
+++ Python/search-a-2d-matrix-ii.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def searchMatrix(self, matrix, target):
         """
         :type matrix: List[List[int]]
@@ -8,5 +8,30 @@ class Solution:
         
         for row in matrix:
             if target in row:
+                return True
+        return False
+
+class Solution2:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+
+        # setup bottom left
+        row_num = len(matrix)
+        col_num = len(matrix[0])
+
+        row, col = len(matrix)-1, 0
+        while row >= 0 and col < col_num:
+            if matrix[row][col] < target:
+                col = col + 1
+            elif matrix[row][col] > target:
+                row = row-1
+            else:
                 return True
         return False

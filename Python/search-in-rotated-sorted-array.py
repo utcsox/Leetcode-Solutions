cd6e541f9f472550939fc5c7567aa7493f3a41b3
Chris Chen <utcsox@gmail.com>
2020-06-11 20:26:14 -0700

    Update search-in-rotated-sorted-array.py
    Add #33 search in rotated sorted array with one pass binary solution

--- Python/search-in-rotated-sorted-array.py
+++ Python/search-in-rotated-sorted-array.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def search(self, nums: List[int], target: int) -> int:
         def find_rotate_index(left, right):
             if nums[left] < nums[right]:
@@ -43,4 +43,65 @@ class Solution:
             return binary_search(rotate_index, n-1)
         
         return binary_search(0, rotate_index)
-        
+   
+class Solution2:
+
+    def search(self, nums: List[int], target: int) -> int:
+
+        #sortednums = sorted(nums)
+
+        beg, end = 0 , len(nums)-1
+
+        while beg <= end:
+
+            mid = (beg + end)//2
+
+           
+
+            if nums[mid] == target:
+
+                return mid
+
+           
+
+            # if mid point is larger and equals to the beginning element of the array
+
+                 # + if target in between nums[start] & nums[mid]
+
+                 # + if target is greater than nums[start] & nums[mid]
+
+                   
+
+            if nums[beg] <= nums[mid]:
+
+                if target < nums[mid] and target >= nums[beg]:
+
+                    end = mid-1
+
+                else:
+
+                    beg = mid + 1                 
+
+            # if midpoint is less than the beg element of the array
+
+                 # + if target is in beteen Nums[mid] and nums[end]
+
+                 # + if target is less than both nums[mid] and nums[end]
+
+                   
+
+            else:
+
+                if target<= nums[end] and target > nums[mid]:
+
+                    beg = mid+1
+
+                else:
+
+                    end = mid-1
+
+           
+
+        return -1
+
+

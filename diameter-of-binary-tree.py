460059f23ea913a52ea663ccff98c25c9d2f16e7
Chris Chen <utcsox@gmail.com>
2020-06-18 22:43:35 -0700

    Create diameter-of-binary-tree.py
    Add #543 Diameter of Binary Tree solution

---
+++ diameter-of-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        def helper(node, path):
+            if not node:
+                return
+            path += str(node.val)
+            if not node.left and not node.right:
+                result.append(path)
+            if node.left:
+                helper(node.left, path + '->')
+                
+            if node.right:
+                helper(node.right, path + '->')
+                    
+        result = []
+
+        
+        helper(root, '')
+        
+        return result

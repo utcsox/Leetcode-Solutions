1b6c7761afdcc6015f4199caa206c5ea71f611e8
Chris Chen <utcsox@gmail.com>
2020-06-19 19:06:53 -0700

    Update add-binary.py
    # 67 Add Binary -> Add Bit manipulation techniques with XOR and bit shifting

--- Python/add-binary.py
+++ Python/add-binary.py
@@ -21,3 +21,12 @@ class Solution2:
         if carry != 0: 
             result = result + str(carry)
         return result[::-1]
+    
+class Solution3:
+    def addBinary(self, a: str, b: str) -> str:
+        x, y = int(a, 2), int(b, 2)
+        
+        while y:
+            x, y = x^y , (x&y)<<1
+            
+        return bin(x)[2:]

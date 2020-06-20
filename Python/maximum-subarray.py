a54496401bf3d3fa7994648c7fc59a089b97fdfd
Chris Chen <utcsox@gmail.com>
2020-06-20 15:38:19 -0700

    Update maximum-subarray.py
    Add #53 Maximum subarray solution #2

--- Python/maximum-subarray.py
+++ Python/maximum-subarray.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def maxSubArray(self, nums: List[int]) -> int:
         if max(nums) < 0:
             return max(nums)
@@ -7,3 +7,17 @@ class Solution:
             local_max = max(0, local_max + num)
             global_max = max(local_max, global_max)
         return global_max
+    
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        #initialize an array
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        
+        for index in range(1, len(nums)):
+            dp[index] = max(dp[index-1] + nums[index], nums[index])
+            
+        return max(dp)

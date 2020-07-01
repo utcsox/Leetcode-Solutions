53ba1ffd8535150a4296111aed605686d9c06428
Chris Chen <utcsox@gmail.com>
2020-06-30 19:14:41 -0700

    Update subsets.py
    Add #78 Subsets solution 2

--- Python/subsets.py
+++ Python/subsets.py
@@ -14,4 +14,29 @@ class Solution1:
         helper(nums, [], 0)
         
         return result
+    
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        def dfs(first, item):
+
+            #if the combination is done 
+            if len(item) == k:
+                output.append(item[:])
+
+            for index in range(first, n):
+                # Add nums[index] into the current combination
+                item.append(nums[index])
+                # Use the next integers to complete the combination
+                dfs(index + 1, item)
+                # backtrack
+                item.pop()
+
+        output = []
+        n = len(nums)
+
+        for k in range(n+1):
+            dfs(0, [])
+
+        return output
             

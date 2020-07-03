61c737bded74eae044c154150164c1c7b3ab1016
Chris Chen <utcsox@gmail.com>
2020-07-03 16:48:34 -0700

    Update merge-sorted-array.py
    88.  Merge Sorted Array:  Add two-pointer solution

--- Python/merge-sorted-array.py
+++ Python/merge-sorted-array.py
@@ -8,3 +8,32 @@ class Solution1:
             nums1[index]= nums2[i]
             i = i + 1
         nums1.sort()
+        
+        
+ class Solution2:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # two pointer solution
+        #1. make a copy of nums1
+        #2. merge nums1 and nums2
+        
+        copy = nums1[:m]
+        nums1[:] = []
+        p1, p2 = 0, 0
+        
+        while p1<m and p2<n:
+            if copy[p1] < nums2[p2]:
+                nums1.append(copy[p1])
+                p1 += 1
+                
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+                
+        if p1< m:
+            nums1[p1+p2: ] = copy[p1:]
+            
+        else:
+            nums1[p1+p2:] = nums2[p2:]

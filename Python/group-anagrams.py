8b1cfe0ac13478256bb0d5760f1fcaceef4fd7b3
Chris Chen <utcsox@gmail.com>
2020-07-03 13:05:14 -0700

    Update group-anagrams.py
    Update #49 Group anagrams with comments.

--- Python/group-anagrams.py
+++ Python/group-anagrams.py
@@ -1,10 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        #.  if the sorted strings are the same, they are anagrams.
         anagrams_map, result = collections.defaultdict(list), []
+        
+        #1.  store same anagrams in the defaultdict
         for s in strs:
-            sorted_str = ("").join(sorted(s))
-            anagrams_map[sorted_str].append(s)
+            sorted_string = ''.join(sorted(s))
+            anagrams_map[sorted_string].append(s)
+        
+        #. sort the list of strings of same anagram
         for anagram in anagrams_map.values():
             anagram.sort()
             result.append(anagram)
+        
         return result

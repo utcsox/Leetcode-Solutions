b9aa63c1fd4dce535f8033be4a5bbae2a7a9008e
Chris Chen <utcsox@gmail.com>
2020-07-03 10:41:21 -0700

    Update next-permutation.py
    Update #31 Next permutation with comments

--- Python/next-permutation.py
+++ Python/next-permutation.py
@@ -2,17 +2,25 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
-        k, l = -1, 0
-        for i in range(len(nums)-1):
-            if nums[i] < nums[i+1]:
-                k = i
-        if k== -1:
-            nums.reverse()
-            return
+        """  
+        k = - 1
         
+        # 1.  Find the element stop increasing as k
+        for index in range(len(nums)-1):
+            if nums[index] < nums[index+1]:
+                k = index
+                
+        # if k is sill - 1, it means we need to reverse the number
+        if k == - 1:
+            return nums.reverse()
+        
+        # 2. Identity the element from k+1 to the end that is bigger than k
+        sindex = 0
         for index in range(k+1, len(nums)):
             if nums[index] > nums[k]:
-                l = index
-        nums[k], nums[l] = nums[l], nums[k]
-        nums[k+1:] = nums[:k:-1]
+                sindex = index
+                 
+        # 3.  Swap the element from #2 with the element from k
+        # 4.  reverse the remaining elements from k+1:
+        nums[k], nums[sindex] = nums[sindex], nums[k] 
+        nums[k+1: ] = nums[:k:-1]

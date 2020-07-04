939c9c337a4dbecc53dfc069d15dfe92ba1864be
Chris Chen <utcsox@gmail.com>
2020-07-03 18:11:45 -0700

    Update valid-palindrome.py
    #125 Valid Palindrome Update Solution 1 with comment

--- Python/valid-palindrome.py
+++ Python/valid-palindrome.py
@@ -1,9 +1,12 @@
 class Solution:
     def isPalindrome1(self, s: str) -> bool:
+        # two-pointer approach
+        # 1.  convert the string to lower case
+        # 2.  Setup two pointers: one from beg and one from end
+        # 3.  if both pointer meet, then it is a palindrome
         string = s.lower()
         beg, end = 0, len(string)-1
         while beg < end:
-            print(beg, end)
             if s[beg].isalnum() == False:
                 beg += 1
             

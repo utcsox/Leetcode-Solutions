f2babfa5af7f41043cb8e6e5acf6fc35d1d246e9
Chris Chen <utcsox@gmail.com>
2020-07-05 11:33:32 -0700

    Update subarray-sum-equals-k.py
    #560 Subarray Sum Equals K - Update with Brute Force Solution

--- Python/subarray-sum-equals-k.py
+++ Python/subarray-sum-equals-k.py
@@ -1,5 +1,21 @@
-class Solution:
+# Brute Force -> not accepted
+# O(3) time , O(n) space
+# get the sums of every i, j
+
+class Solution1:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        cnt = 0
+        
+        for i in range(N):
+            for j in range(i, N):
+                summation = 0
+                for l in range(i, j+1):
+                    summation += nums[l]
+                if summation == k:
+                    cnt += 1
+        return cnt    
+    def subarraySum3(self, nums: List[int], k: int) -> int:
         
         count = 0
         sum_val = 0

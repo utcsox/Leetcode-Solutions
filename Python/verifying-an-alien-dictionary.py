edd5a5f7dc81126579836cbbb4c7491b12923498
Chris Chen <utcsox@gmail.com>
2020-07-05 21:31:25 -0700

    Update verifying-an-alien-dictionary.py
    #953 Verifying an Alien Dictionary:  Update code with comments

--- Python/verifying-an-alien-dictionary.py
+++ Python/verifying-an-alien-dictionary.py
@@ -1,24 +1,30 @@
 class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
-        table_lookup = {}
+    def isAlienSorted(self, words: List[str], order: str) -> bool:    
+        # 1. create a lookup dictionary of the order of character
+        lookup = {char: index for index, char in enumerate(order)}
+        print(lookup)
         
-        # Create a lookup dictionary
-        for index, char in enumerate(order):
-            table_lookup[char] = index
-        
-        # loop thru the words list     
+        # 2. compare two words, characters by characterz.
+        # 3.  when first/second characters are different:
+        #     a). if first word char < second word char ->break
+        #    b). else first word char > second word char -> return False
+        # 4.  When the loop is complete, if the first word is longer than second world - > return False
+        # 5.  Return True out of for loop
+    
         for index in range(len(words)-1):
-            first_word = words[index]
-            second_word = words[index + 1]
+            first = words[index]
+            second = words[index + 1]
             
-            for j in range(min(len(first_word), len(second_word))):
-                if first_word[j] != second_word[j]:
-                    # table lookup
-                    if table_lookup[first_word[j]] > table_lookup[second_word[j]]:
+            for j in range(min(len(first), len(second))):
+                if first[j] != second[j]:
+                   
+                    if lookup[first[j]] > lookup[second[j]]:
+                        print(j, 'adotheo')
                         return False
+                    
                     break
             else:
-                if len(first_word) > len(second_word):
+                if len(first) > len(second):
                     return False
                 
         return True

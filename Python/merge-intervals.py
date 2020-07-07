a83f7fb80f80b0a96a7c3c70e407e4bcacd89d5d
Chris Chen <utcsox@gmail.com>
2020-07-06 22:42:47 -0700

    Update merge-intervals.py
    #56 Merge Intervals:  Update code with comments

--- Python/merge-intervals.py
+++ Python/merge-intervals.py
@@ -1,21 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # One can merge two element together if the beginning of the second element is smaller than the 
+        # end of the first element
+        # 1.  sort the array by its first element
+        # 2.  To merge overlap interval:
+        # 3.  i) if there is no overlap between the previous/current array, add the current array to output
+        # 4.  ii) if there is overlap, the last interval end element is equal to   or curent end element)
         
-        results = []
-
+        result = []
+        if not intervals:
+            return result
+        
         intervals.sort(key = lambda x: x[0])
+        result.append(intervals[0])
         
-        for interval in intervals:
-            # if the output is empty, add the first element in there
-            if not results:
-                results.append(interval)
-            # if no overlap
+        for index in range(1, len(intervals)):
             
-            elif results[-1][1] < interval[0]:
-                results.append(interval)
-                
-            # if overlap
+            # if no overlap
+            if result[-1][1] < intervals[index][0]:
+                result.append(intervals[index])
             else:
-                results[-1][1] = max(results[-1][1], interval[1])
-                
-        return results
+                result[-1][1] = max(intervals[index][1], result[-1][1])
+        
+        return result

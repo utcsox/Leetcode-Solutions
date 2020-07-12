ed1707d3887e4d79ccebfac32f8fe13a466981da
Chris Chen <utcsox@gmail.com>
2020-07-11 18:30:51 -0700

    Update top-k-frequent-elements.py
    #347 Top K Frequent Element:  Add solution with sort methods

--- Python/top-k-frequent-elements.py
+++ Python/top-k-frequent-elements.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         # if the length of list is equals to k return the original list
@@ -16,3 +16,21 @@ class Solution:
         
         return klargest 
     
+class Solution2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # if the length of list is equals to k return the original list
+        if len(nums) == k:
+            return nums
+        
+        #  Use counter data structure to store the count of each integer       
+        counter = collections.Counter(nums)
+        
+        # Sort the counter object by value
+        result = [] 
+        result = list(sorted(counter.items(), key=lambda x: x[1], reverse=True))
+        
+        # store the top k results
+        result= [x[0] for x in result]
+        
+        return result[:k]

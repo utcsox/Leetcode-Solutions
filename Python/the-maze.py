7fcc7c27debc13015ff6899ceeea2bb35520e08b
Chris Chen <utcsox@gmail.com>
2020-08-16 12:58:34 -0700

    Update the-maze.py
    490:  The maze:  Add BFS solution

--- Python/the-maze.py
+++ Python/the-maze.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
         
         # Define total rows, columns, visited points, and a queue      
@@ -27,4 +27,42 @@ class Solution:
                     stack.append((new_row, new_column))
                     visited.add((new_row, new_column))
                 
+        return False
+
+    
+class Solution2:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        
+     # Define total rows, columns, visited points, and a queue      
+        visited = set()
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        rows, columns = len(maze), len(maze[0])
+        
+        # Add the starting point
+        visited.add((start[0], start[1]))
+        queue = deque([start])
+        
+        while queue:
+            
+            x, y = queue.popleft()
+            for direction in directions:
+                nx = x + direction[0] 
+                ny = y + direction[1]
+                
+                while 0 <= nx < rows and 0 <= ny < columns and maze[nx][ny] != 1:
+                    nx += direction[0]
+                    ny += direction[1]
+                        
+                # need to backtrack
+                
+                nx -= direction[0]
+                ny -= direction[1]
+                
+                if [nx, ny] == destination:
+                    return True
+                
+                if (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    queue.append((nx, ny))
+                    
         return False

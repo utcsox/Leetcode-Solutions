ad39406ca98afdf86258fceaf7073312fe8339ea
Chris Chen <utcsox@gmail.com>
2020-08-16 18:07:32 -0700

    Update lru-cache.py
    # 146.  LRU Cache:  Add OrderedDict solution

--- Python/lru-cache.py
+++ Python/lru-cache.py
@@ -34,8 +34,6 @@ class LRUCache:
         self.cache[key] = value
         self.order.append(key)
   
-  
-  *************************************************************************
         # if the length of queue is greater than capacity, remove the first element       
         if len(self.order) > self.capacity:
             oldest = self.order.popleft()
@@ -47,3 +45,33 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+*************************************************************************
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+      
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return - 1
+        
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+        
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+    
+    
+    

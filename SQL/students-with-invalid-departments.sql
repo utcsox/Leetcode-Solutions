2ddd8b9076bf242c8a2fc5fe9d4b3c3347aa9dcf
Chris Chen <utcsox@gmail.com>
2020-11-10 20:21:23 -0800

    Update students-with-invalid-departments.sql
    #1350.  Students with Invalid Departments solution 2 : Left JOIN + Filter

--- SQL/students-with-invalid-departments.sql
+++ SQL/students-with-invalid-departments.sql
@@ -5,3 +5,9 @@ WHERE department_id NOT IN (
     SELECT id
     FROM Departments
 )
+# 2.  LEFT JOIN + Filter
+SELECT Students.id, Students.name
+FROM Students
+LEFT JOIN Departments
+ON students.department_id = Departments.id
+WHERE Departments.id is NULL

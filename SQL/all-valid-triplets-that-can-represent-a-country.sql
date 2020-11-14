bf461802e8fe96b8ef9b6fad90c3b998605dba87
Chris Chen <utcsox@gmail.com>
2020-11-14 07:43:39 -0800

    Update all-valid-triplets-that-can-represent-a-country.sql
    # 1623 1623. All Valid Triplets That Can Represent a Country Add CROSS JOIN solution

--- SQL/all-valid-triplets-that-can-represent-a-country.sql
+++ SQL/all-valid-triplets-that-can-represent-a-country.sql
@@ -7,3 +7,14 @@ AND SchoolB.student_id != SchoolC.student_id
 AND SchoolA.student_name != SchoolB.student_name
 AND SchoolA.student_name != SchoolC.student_name
 AND SchoolB.student_name != SchoolC.student_name
+
+
+# Write your MySQL query statement below
+SELECT SchoolA.student_name AS member_A, SchoolB.student_name AS member_B, SchoolC.student_name AS member_C
+FROM SchoolA CROSS JOIN SchoolB CROSS JOIN SchoolC
+WHERE SchoolA.student_id != SchoolB.student_id
+AND SchoolA.student_id != SchoolC.student_id
+AND SchoolB.student_id != SchoolC.student_id
+AND SchoolA.student_name != SchoolB.student_name
+AND SchoolA.student_name != SchoolC.student_name
+AND SchoolB.student_name != SchoolC.student_name

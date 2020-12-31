f4eeca89f60230e2cae15f552e5acec105eb85f6
Chris Chen <utcsox@gmail.com>
2020-12-30 22:34:12 -0800

    Update customer-who-visited-but-did-not-make-any-transactions.sql
    SQL #1581 Customer Who Visited but Did Not Make Any Transaction -> Add NOT IN solution

--- SQL/customer-who-visited-but-did-not-make-any-transactions.sql
+++ SQL/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -6,3 +6,11 @@ LEFT JOIN Transactions
 ON Visits.visit_id = Transactions.visit_id
 WHERE Transactions.visit_id is NULL
 GrOUP BY Visits.customer_id
+
+
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits
+WHERE Visits.visit_id NOT IN (SELECT Transactions.visit_id
+                              FROM Transactions)
+
+GROUP BY Visits.customer_id

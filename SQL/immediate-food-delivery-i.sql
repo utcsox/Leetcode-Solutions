83d6f82ac5f054c13e9ef40ae33264aa5f5c8bce
Chris Chen <utcsox@gmail.com>
2021-01-01 10:30:58 -0800

    Update immediate-food-delivery-i.sql
    #1173 Immediate Food Delivery Add CTE method

--- SQL/immediate-food-delivery-i.sql
+++ SQL/immediate-food-delivery-i.sql
@@ -3,3 +3,12 @@
 SELECT ROUND(100*SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1
             ELSE 0 END)/COUNT(1), 2) AS 'immediate_percentage'
 FROM Delivery 
+
+WITH cte AS
+    (
+    SELECT CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1
+                ELSE 0 END AS immediate_or_scheduled
+    FROM Delivery AS d)
+    
+Select ROUND(100*SUM(immediate_or_scheduled)/COUNT(immediate_or_scheduled), 2) AS immediate_percentage
+from cte

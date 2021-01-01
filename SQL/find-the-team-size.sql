39c0feeb259acaa5ce86cb030200bf721bf70d46
Chris Chen <utcsox@gmail.com>
2020-12-31 17:17:40 -0800

    Update find-the-team-size.sql
    Add 1303. Find the Team Size ->Windows methods

--- SQL/find-the-team-size.sql
+++ SQL/find-the-team-size.sql
@@ -6,3 +6,7 @@ JOIN (SELECT team_id, COUNT(team_id) AS team_size
       FROM Employee 
       GROUP BY team_id) AS e2
 WHERE e1.team_id = e2.team_id
+
+
+SELECT employee_id, COUNT(employee_id) OVER (PARTITION BY team_id AS team_size
+FROM Employee

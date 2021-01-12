2634229a6ed041493be262e0f06f436195486fd6
Chris Chen <utcsox@gmail.com>
2021-01-11 19:30:57 -0800

    Update user-activity-for-the-past-30-days-i.sql
    #1141.  User Activity for the Past 30 Days I using DATEDIFF

--- QL/user-activity-for-the-past-30-days-i.sql
+++ QL/user-activity-for-the-past-30-days-i.sql
@@ -5,3 +5,10 @@ FROM Activity a
 WHERE a.activity_date BETWEEN DATE_ADD('2019-07-27', INTERVAL -29 DAY) AND '2019-07-27'
 GROUP BY a.activity_date 
 Having active_users> 0
+
+
+SELECT a.activity_date AS day, COUNT(DISTINCT a.user_id) AS active_users
+FROM Activity a
+WHERE DATEDIFF('2019-07-27', a.activity_date ) < 30
+GROUP BY a.activity_date 
+Having active_users> 0

0fcfd53d04cf901633f09d8845f121a25eddb431
Chris Chen <utcsox@gmail.com>
2021-01-12 21:37:09 -0800

    Create sales-analysis-ii.sql
    Add #1083 Sales Analysis II query

---
+++ SQL/sales-analysis-ii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+WITH CTE AS (SELECT DISTINCT s.buyer_id
+             FROM Sales s
+             JOIN Product p 
+             ON s.product_id = p.product_id
+             WHERE product_name = 'iPhone'
+             GROUP BY s.buyer_id)
+             
+SELECT buyer_id
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
+WHERE product_name = 'S8'
+GROUP BY buyer_id 
+Having buyer_id NOT IN (SELECT buyer_id FROM CTE)

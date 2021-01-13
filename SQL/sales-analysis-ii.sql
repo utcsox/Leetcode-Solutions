a623bde21bda75b434498c092331f78a7f8df313
Chris Chen <utcsox@gmail.com>
2021-01-12 21:43:33 -0800

    Update sales-analysis-ii.sql
    Add #1083 Sales Analysis II without sub-query

--- SQL/sales-analysis-ii.sql
+++ SQL/sales-analysis-ii.sql
@@ -14,3 +14,12 @@ ON s.product_id = p.product_id
 WHERE product_name = 'S8'
 GROUP BY buyer_id 
 Having buyer_id NOT IN (SELECT buyer_id FROM CTE)
+
+
+             
+SELECT buyer_id
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY buyer_id 
+Having SUM(p.product_name= 'S8') > 0 AND SUM(p.product_name ='iPhone') = 0 

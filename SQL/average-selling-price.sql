ac669837fcc92d3b0cd14c4a058daabe94cf1ae0
Chris Chen <utcsox@gmail.com>
2021-01-17 10:20:13 -0800

    Update average-selling-price.sql
    #1251.  Average Selling Price - Add query using Window function

--- SQL/average-selling-price.sql
+++ SQL/average-selling-price.sql
@@ -6,3 +6,11 @@ ON u.product_id = p.product_id
 WHERE u.purchase_date >= p.start_date 
 AND u.purchase_date <= p.end_date
 GROUP BY p.product_id
+
+
+SELECT DISTINCT p.product_id, 
+      ROUND((SUM(p.price*u.units) OVER (PARTITION BY p.product_id))/(SUM(u.units) OVER (PARTITION BY p.product_id)), 2) AS average_price
+FROM Prices p
+JOIN UnitsSold u
+ON p.product_id = u.product_id
+WHERE u.purchase_date >= p.start_date AND u.purchase_date <= p.end_date

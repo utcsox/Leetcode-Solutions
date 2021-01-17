b66b52ea5fa27746f4fc573b277317995e505c32
Chris Chen <utcsox@gmail.com>
2021-01-17 14:46:09 -0800

    Update rotate-array.py
    Add #0189 Rotate Array with reverse method

--- Python/rotate-array.py
+++ Python/rotate-array.py
@@ -10,3 +10,21 @@ class Solution1:
             tmp[(i+k)%len(nums)] = nums[i]
         
         nums[:] = tmp
+        
+        
+class Solution2:
+    def reverse(self, nums, beg, end):
+        while(beg < end):
+            nums[beg], nums[end] = nums[end], nums[beg]
+            beg, end = beg + 1, end - 1
+            
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k%=n
+        
+        self.reverse(nums, 0, n-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, n-1)

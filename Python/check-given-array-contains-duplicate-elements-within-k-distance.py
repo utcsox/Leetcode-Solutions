30450f99b65a6e8c19b35fa08f30dfe0e78fd40d
Chris Chen <utcsox@gmail.com>
2021-01-19 22:16:06 -0800

    Update check-given-array-contains-duplicate-elements-within-k-distance.py
    Update #9996 Given Array Contains Duplicate Elements within K distance using Hashing -> O(n) solution

--- Python/check-given-array-contains-duplicate-elements-within-k-distance.py
+++ Python/check-given-array-contains-duplicate-elements-within-k-distance.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def checkDuplicatesWithinK(self, arr: List, k:int)->bool:
         for i in range(0, len(arr)):
             for j in range(i+1, len(arr)):
@@ -6,5 +6,19 @@ class Solution:
                     break
                 elif arr[i] == arr[j]:
                     return True
+                
+        return False
+
+    
+class Solution2:
+    def checkDuplicatesWithinK(self, arr: List, k:int)->bool:
+        tmp = []
+        for i in range(0, len(arr)):
+            if arr[i] in tmp:
+                return True
+            
+            tmp.append(arr[i])
+            if i >=k:
+                tmp.remove(arr[i-k])
                 
         return False

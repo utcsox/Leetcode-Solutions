166fa7842ed8bb1423fcf2ca5614a10a8edbd514
Chris Chen <utcsox@gmail.com>
2021-01-23 13:38:10 -0800

    Update longest-common-prefix.py
    Add #14 Longest Common Prefix solution - Python

--- Python/longest-common-prefix.py
+++ Python/longest-common-prefix.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
@@ -8,3 +8,30 @@ class Solution:
                 if i>=len(j) or strs[0][i] != j[i]:
                     return strs[0][0:i]
         return strs[0]
+
+    
+ class Solution2:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if not strs:
+            return ''
+        
+        if len(strs) == 1:
+            return strs[0]
+        
+        max_len = max([len(str) for str in strs])
+        min_len = min([len(str) for str in strs])
+        
+        output = ''
+        for i in range(max_len):
+            if i < min_len:
+                tmp = [1 if str[i] == strs[0][i] else 0 for str in strs]
+                if all(tmp):
+                    output += strs[0][i]
+                
+                else:
+                    return output
+            else:
+                return output
+            
+        return output

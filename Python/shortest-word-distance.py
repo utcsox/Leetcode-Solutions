fbe0a6629c942c01d77dbd7444db47105f934937
Chris Chen <utcsox@gmail.com>
2021-09-28 19:21:53 -0700

    Update shortest-word-distance.py
    Add #243.  Shortest Word Distance one-pass solution

--- Python/shortest-word-distance.py
+++ Python/shortest-word-distance.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         
         tmp = defaultdict(list)
@@ -16,6 +16,27 @@ class Solution:
         
         for p1 in tmp[word1]:
             for p2 in tmp[word2]:
+                output = min(output, abs(p1-p2))
+                
+        return output
+
+ class Solution2:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        
+        p1 = -1
+        p2 = -1
+        
+        output = 2**31-1
+        
+        for index in range(len(wordsDict)):
+            if word1 == wordsDict[index]:
+                p1 = index
+                
+            if word2 == wordsDict[index]:
+                p2 = index
+                
+            
+            if p1 != -1 and p2 != -1:
                 output = min(output, abs(p1-p2))
                 
         return output

d33f49600149e63becb2ae388adabd8ee2dcb536
Chris Chen <utcsox@gmail.com>
2021-10-04 23:19:37 -0700

    Update number-of-islands.py
    Add BFS solution to 200. Number of Islands

--- Python/number-of-islands.py
+++ Python/number-of-islands.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def numIslands(self, grid: List[List[str]]) -> int:
         
         if not grid:
@@ -36,4 +36,38 @@ class Solution:
                     helper(grid, r, c)
         return count
                     
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
         
+        if not grid:
+            return 0
+        
+        rows, columns = len(grid), len(grid[0])
+        islands = 0
+        visited = set()
+        directions=[(0, 1), (1, 0), (-1, 0), (0, -1)]
+        
+        def bfs(r, c):
+            queue = collections.deque()
+            visited.add((r, c))
+            queue.append((r, c))
+            
+            while queue:
+                row, column = queue.popleft()
+                for dr, dc in directions:
+                    if (row + dr in range(rows) and
+                       column +dc in range(columns) and 
+                       grid[row + dr][column + dc] == '1' and
+                       (row + dr, column +dc) not in visited):
+                        queue.append((row + dr, column +dc))
+                        visited.add((row+dr, column+dc))
+                    
+                
+        
+        for r in range(rows):
+            for c in range(columns):
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+                    
+        return islands        

ddfad3955fe43c5f717facdc20429c7e860fed05
Chris Chen <utcsox@gmail.com>
2021-11-04 10:51:50 -0700

    Update the-kth-factor-of-n.py
    1492.  The kth Factor of n Add O(log n) solution

--- Python/the-kth-factor-of-n.py
+++ Python/the-kth-factor-of-n.py
@@ -11,3 +11,25 @@ class Solution1:
         else:
             return -1
                 
+class Solution2:
+    def kthFactor(self, n: int, k: int) -> int:
+        
+        output = []
+        sqrt_n = int(n**0.5)
+        
+        for index in range(1, sqrt_n+1):
+            if n % index == 0:
+                output.append(n//index)
+                k -= 1
+                
+                if k == 0:
+                    return index
+            
+        if output[-1]**2== n:
+            output.pop()
+
+        if len(output) < k:
+            return -1
+        
+        else:
+            return output[-k]

e10d96ed334c7935d716d8a14e2b11798fc95ba6
Chris Chen <utcsox@gmail.com>
2021-11-09 15:45:46 -0800

    Update insert-interval.py
    Add a better solution for 57.  Insert Interval

--- Python/insert-interval.py
+++ Python/insert-interval.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
         output = []
@@ -21,5 +21,31 @@ class Solution:
                 output.append([start,end])
             else:
                 output[-1][1] = max(output[-1][1], end)
+        
+        return output
+    
+class Solution2:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        output = []
+        
+        intervals.sort(key = lambda x: x[0])
+        
+        for index in range(len(intervals)):
+            
+            #if end value of new interval is smaller than start value of current interval
+            if newInterval[1] < intervals[index][0]:
+                output.append(newInterval)
+                return output + intervals[index:]
+            
+            # if start value of new interval is larger than end value of current interval
+            elif newInterval[0] > intervals[index][1]:
+                output.append(intervals[index])
+            
+            # if there is overlap
+            else:
+                newInterval = [min(newInterval[0], intervals[index][0]), max(newInterval[1], intervals[index][1])]
+                
+        output.append(newInterval)
         
         return output

7cd7a11d4f8f312fb2370ad65238f4954e1617b0
Chris Chen <utcsox@gmail.com>
2022-01-10 20:40:20 -0800

    Create encode-and-decode-strings.py
    Add 0271 Encode and Decode Strings  solution

---
+++ Python/encode-and-decode-strings.py
@@ -0,0 +1,36 @@
+class Codec:
+    def encode(self, strs: [str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        output = ''
+        for st in strs:
+            output += str(len(st)) + '#' + st
+            
+        return output
+            
+            
+
+    def decode(self, s: str) -> [str]:
+        """Decodes a single string to a list of strings.
+        """
+        
+        output , i = [], 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+                
+            length = int(s[i:j])
+            output.append(s[j + 1: j +1 + length])
+            
+            i = j+1+length
+            
+        return output
+        
+
+
+# Your Codec object will be instantiated and called as such:
+strs = ['Delta', 'Omnicron']
+codec = Codec()
+codec.decode(codec.encode(strs))

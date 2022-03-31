9dce856263768ef24cf222cea5271908124b4ee8
Chris Chen <utcsox@gmail.com>
2022-03-31 16:06:24 -0700

    Create percentage-of-users-attended-a-contest.sql
    Add query for #1633.  Percentage of Users Attended a Contest

---
+++ SQL/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,4 @@
+SELECT R.contest_id, ROUND(COUNT(DISTINCT R.user_id)/(SELECT COUNT(DISTINCT user_id) FROM Users) * 100, 2) AS percentage
+FROM Register R
+GROUP BY R.contest_id
+ORDER BY percentage DESC, contest_id ASC

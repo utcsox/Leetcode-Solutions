016c4cf44aaa6bd8d852aa52b8caae69656caa56
Chris Chen <utcsox@gmail.com>
2022-06-07 21:18:54 -0700

    Create the-number-of-users-that-are-eligible-for-discount.sql
    Add query for #2205.  The Number of Users That Are Eligible for Discount

---
+++ SQL/the-number-of-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT COUNT(DISTINCT user_id) AS user_cnt
+      FROM Purchases
+      WHERE amount >= minAmount and time_stamp BETWEEN startDate AND endDate  
+  );
+END

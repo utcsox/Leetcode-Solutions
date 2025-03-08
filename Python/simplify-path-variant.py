21be3c58d69eab91681aee7cbe5a90c8696a3319
Chris Chen <utcsox@gmail.com>
2025-03-07 22:43:07 -0800

    Add 0071v Simplify Path Variant solution

---
+++ Python/simplify-path-variant.py
@@ -0,0 +1,43 @@
+class Solution:
+    def simplifyPathVariant(self, cwd: str, cd: str) -> str:
+      if not cd:
+        return cwd
+      if cd.startswith("/"):
+        cd = ""
+      cwd_tokens = cd.split("/")
+      tokens = [for token in cwd_tokens if token]
+
+      for token in cd.split("/"):
+        if not token:
+            continue
+        if token == '.':
+            continue
+        if token == '..':
+            if tokens:
+                tokens.pop()
+        else:
+            tokens.append(token)
+
+      if not tokens:
+          return "/"
+
+      result = "/" + "/".join(tokens)
+      return result
+
+
+def test_simplifyPathVariant():
+    cwd = "/a/b/c", 
+    cd = "/d/./e"  
+    assert Solution().simplifyPathVariant(cwd, cd) == "/d/e"
+
+    cwd = ""
+    cd = "/d/./e" 
+    assert Solution().simplifyPathVariant(cwd, cd)== "/d/e"
+
+    cwd = "/a/b/c"
+    cd = "" 
+    assert Solution().simplifyPathVariant(cwd, cd)== "/a/b/c"
+
+    cwd = "/a/b"
+    cd = ".//c/../../d/f"
+    assert Solution().simplifyPathVariant(cwd, cd) == "/a/d/f"

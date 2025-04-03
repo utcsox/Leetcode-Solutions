a40d5d13ebc76fc1a85cb390fb993527b0e5caf5
Chris Chen <utcsox@gmail.com>
2025-04-03 09:36:40 -0700

    Update k-closest-points-to-origin.py with Heap solution

--- Python/k-closest-points-to-origin.py
+++ Python/k-closest-points-to-origin.py
@@ -46,3 +46,17 @@ class Solution2:
                 
         sort(0, len(points)-1, K)        
         return points[:K]
+
+class Solution3:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        heap = []
+        for x, y in points:
+            d = math.sqrt(x**2 + y**2)
+            heapq.heappush(heap, (-d, x, y))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [[x, y] for _, x, y in heap]
+   

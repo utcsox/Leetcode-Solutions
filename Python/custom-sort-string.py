c8c1a979f684caec40676f211c2e4ec117f2222a
Chris Chen <utcsox@gmail.com>
2025-04-05 22:53:11 -0700

    Add solution with no hashmap to custom-sort-string.py

--- Python/custom-sort-string.py
+++ Python/custom-sort-string.py
@@ -15,3 +15,19 @@ class Solution:
             output += k * v
 
         return output
+
+class Solution2:
+    def customSortString(self, order: str, s: str) -> str:
+        freqs = [0] * 26
+        for ch in s:
+            freqs[ord(ch) - ord('a')] += 1
+
+        result = ""
+        for ch in order:
+            result += ch * freqs[ord(ch) - ord('a')]
+            freqs[ord(ch) - ord('a')] = 0
+
+        for i in range(26):
+            result += chr(i + ord('a')) * freqs[i]
+
+        return result

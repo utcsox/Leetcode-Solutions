00210132f6b612346fe868fdfa2cbde8de9207a5
Chris Chen <utcsox@gmail.com>
2025-04-12 21:05:51 -0700

    Update merge-sorted-array.py

--- Python/merge-sorted-array.py
+++ Python/merge-sorted-array.py
@@ -15,25 +15,21 @@ class Solution1:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # two pointer solution
-        #1. make a copy of nums1
-        #2. merge nums1 and nums2
-        
-        copy = nums1[:m]
-        nums1[:] = []
-        p1, p2 = 0, 0
-        
-        while p1<m and p2<n:
-            if copy[p1] < nums2[p2]:
-                nums1.append(copy[p1])
-                p1 += 1
-                
+        last = m + n - 1
+
+        while m > 0 and n > 0:
+
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+
             else:
-                nums1.append(nums2[p2])
-                p2 += 1
-                
-        if p1< m:
-            nums1[p1+p2: ] = copy[p1:]
+                nums1[last] = nums2[n - 1]
+                n -= 1
             
-        else:
-            nums1[p1+p2:] = nums2[p2:]
+            last -= 1
+
+        # fill nums1 with leftover nums2 elements
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            last, n = last - 1, n - 1

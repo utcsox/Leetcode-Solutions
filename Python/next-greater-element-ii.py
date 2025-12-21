92350b08b59e5ae0c59c151e23b3c13af79e1e08
Chris Chen <utcsox@gmail.com>
2025-12-20 22:08:13 -0800

    Update next-greater-element-ii.py with stack solution

--- Python/next-greater-element-ii.py
+++ Python/next-greater-element-ii.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         res = [-1 for i in range(len(nums))]
         
@@ -9,3 +9,20 @@ class Solution:
                     break
 
         return res
+
+class Solution2:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        res = [-1 for i in range(len(nums))]
+
+        stack = []
+
+        for i in range(len(nums) * 2):
+            idx = i % len(nums)
+            while stack and nums[idx] > nums[stack[-1]]:
+                j = stack.pop()
+                res[j] = nums[idx]
+
+            if i < len(nums):
+                stack.append(i)
+
+        return res 
